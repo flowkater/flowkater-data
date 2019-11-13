68541a960791598a224a3cd673394a913a822490
flowkater <chjw102@gmail.com>
2019-11-13 12:30:37 +0900

    add date

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -7,7 +7,12 @@ import './index.scss'
 export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
+      <h3>
+        {node.frontmatter.title || node.fields.slug}
+        <time className="thumbnail-date" dateTime={node.frontmatter.date}>
+          {node.frontmatter.date}
+        </time>
+      </h3>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
